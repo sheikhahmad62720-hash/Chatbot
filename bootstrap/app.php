bfd67ad51fdfd3cf5c0d6608b8058490e458aceb
Muhammad Ahmad <sheikhahmad62720@gmail.com>
2026-08-31 20:24:51 +0500

    feat: build real-time one-to-one chat application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->redirectGuestsTo(fn () => route('welcome'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
